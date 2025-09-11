6e02cead800ccc0fc9be39ffd4b808eba9e11ace
srandres-git <andysanchez2001@gmail.com>
2025-09-11 11:22:23 -0600

    Refactor display of 'ejecutivos_cxp' secret to use DataFrame for better formatting

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas as pd
 st.title("Pruebas de Streamlit")
 
 st.write(f'Secreto: {st.secrets["my_secret"]}')
 # imprimimos la tabla 'ejecutivos_cxp' del secreto
-st.write(st.secrets["ejecutivos_cxp"])
+ejecutivos_cxp = st.secrets["ejecutivos_cxp"]
+df = pd.DataFrame(ejecutivos_cxp)
+st.write("Tabla 'ejecutivos_cxp':")

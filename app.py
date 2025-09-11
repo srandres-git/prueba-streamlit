c98484c44bcf6faebe1a43a63f43429400116b77
srandres-git <andysanchez2001@gmail.com>
2025-09-11 11:05:14 -0600

    Add display for 'ejecutivos_cxp' secret in Streamlit app

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title("Pruebas de Streamlit")
 
 st.write(f'Secreto: {st.secrets["my_secret"]}')
+# imprimimos la tabla 'ejecutivos_cxp' del secreto
+st.write(st.secrets["ejecutivos_cxp"])
